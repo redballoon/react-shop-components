8c15aad9b8b9e48b23d7dfdfe0a4976db009d6de
redballoon <redballoon@users.noreply.github.com>
2019-01-24 02:09:45 -0500

    add postcss-loader for autoprefixer; add postcss to config; add support for scss modules;

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
 
 // you can use this file to add your custom webpack plugins, loaders and anything you like.
 // This is just the basic way to add additional webpack configurations.
@@ -8,21 +9,76 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
+
+// store configs for css loader
+const cssLoader = {
+	loader: 'css-loader',
+	options: {
+		modules: false,
+		sourceMap: true,
+		importLoaders: 2,
+	},
+};
+
+const cssModuleLoader = {
+	loader: 'css-loader',
+	options: {
+		modules: true,
+		sourceMap: true,
+		localIdentName: '[local]__[hash:base64:5]',
+		importLoaders: 2,
+	},
+};
+
+// store configs for post css loader
+const postcssLoader = {
+	loader: 'postcss-loader',
+	options: {
+		ident: 'postcss',
+		sourceMap: true,
+		plugins: () => [
+			autoprefixer({
+				browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+			})
+		],
+	},
+};
+
 module.exports = {
 	plugins: [
+		new MiniCssExtractPlugin({
+			filename : 'css/common.css',
+			chunkFilename : 'css/[id].css',
+			// filename : 'css/common.[hash].css',
+			// chunkFilename : 'css/[id].[hash].css',
+		}),
 	],
 	module: {
-		rules : [/*{
+		rules : [{
+			test : /\.module\.s(a|c)ss$/,
+			use : [
+				MiniCssExtractPlugin.loader,
+				// 'style-loader',
+				cssModuleLoader,
+				postcssLoader,
+				'sass-loader',
+			],
+		},
+		//  {
+		// 	test : /\.s(a|c)ss$/,
+		// 	use : [
+		// 		// MiniCssExtractPlugin.loader,
+		// 		'style-loader',
+		// 		cssLoader,
+		// 		postcssLoader,
+		// 		'sass-loader',
+		// 	],
+		// },
+		/*{
 			test : /\.js$/,
 			use : 'babel-loader',
 			exclude : /node_modules/,
-		}, */{
-			test : /\.sass/,
-			use : [
-				MiniCssExtractPlugin.loader,
-				'css-loader',
-				'sass-loader'
-			],
-		}],
+		}, */
+		],
 	},
 };
